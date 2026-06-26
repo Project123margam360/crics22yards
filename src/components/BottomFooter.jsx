1adfa26aa84e54a8aa33d2a720beb89eefd6544d
Project123margam360 <project123.margam360@gmail.com>
2026-06-26 20:02:59 +0530

    Update contact information in BottomFooter component

--- src/components/BottomFooter.jsx
+++ src/components/BottomFooter.jsx
@@ -28,7 +28,7 @@ export default function BottomFooter() {
             </div>
             <div className="contact-item">
               <span className="contact-icon">📞</span>
-              <a href="tel:+16149841453">(614) 984 1453</a>
+              <a href="tel:+17134982155">(713) 498 2155</a>
             </div>
             <div className="contact-item">
               <span className="contact-icon">📍</span>
